8606e5bd77e373fe3937996620ba199032dc3041
Yoonho Aaron Kim <yoonhok524@users.noreply.github.com>
2022-11-27 00:09:04 +0900

    6. Write messages to Firestore

--- get_to_know_firebase_for_flutter/lib/main.dart
+++ get_to_know_firebase_for_flutter/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart'
     hide EmailAuthProvider, PhoneAuthProvider;
 import 'package:firebase_core/firebase_core.dart';
@@ -134,6 +135,18 @@ class HomePage extends StatelessWidget {
             const Paragraph(
               'Join us for a day full of Firebase Workshops and Pizza!',
             ),
+            Consumer<ApplicationState>(
+              builder: (context, appState, child) {
+                return Column(
+                  children: [
+                    const Header('Discussion'),
+                    GuestBook(
+                        addMessage: (message) =>
+                            appState.addMessageToGuestBook(message)),
+                  ],
+                );
+              },
+            ),
           ],
         ),
       ),
@@ -159,5 +172,76 @@ class ApplicationState extends ChangeNotifier {
       _loggedIn = user != null;
       notifyListeners();
     });
+  }
+
+  Future<DocumentReference> addMessageToGuestBook(String message) {
+    if (!_loggedIn) {
+      throw Exception('Must be logged in');
+    }
+
+    return FirebaseFirestore.instance
+        .collection('guestbook')
+        .add(<String, dynamic>{
+      'text': message,
+      'timestamp': DateTime.now().millisecondsSinceEpoch,
+      'name': FirebaseAuth.instance.currentUser!.displayName,
+      'userId': FirebaseAuth.instance.currentUser!.uid,
+    });
+  }
+}
+
+class GuestBook extends StatefulWidget {
+  const GuestBook({required this.addMessage, super.key});
+
+  final FutureOr<void> Function(String message) addMessage;
+
+  @override
+  State<GuestBook> createState() => _GuestBookState();
+}
+
+class _GuestBookState extends State<GuestBook> {
+  final _formKey = GlobalKey<FormState>(debugLabel: '_GuestBookState');
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Form(
+          key: _formKey,
+          child: Row(
+            children: [
+              Expanded(
+                  child: TextFormField(
+                controller: _controller,
+                decoration: const InputDecoration(hintText: 'Leave a message'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Enter your message to continue';
+                  }
+                  return null;
+                },
+              )),
+              const SizedBox(
+                width: 8,
+              ),
+              StyledButton(
+                child: Row(
+                  children: const [
+                    Icon(Icons.send),
+                    SizedBox(width: 4),
+                    Text('SEND')
+                  ],
+                ),
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    await widget.addMessage(_controller.text);
+                    _controller.clear();
+                  }
+                },
+              )
+            ],
+          )),
+    );
   }
 }

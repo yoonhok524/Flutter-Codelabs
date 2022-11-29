ad59e50037941a2776a67f2fbb3dc58f173e1023
Yoonho Aaron Kim <yoonhok524@users.noreply.github.com>
2022-11-29 21:42:31 +0900

    MDC 101 setup

--- mdc_101/lib/home.dart
+++ mdc_101/lib/home.dart
@@ -14,6 +14,22 @@
 
 import 'package:flutter/material.dart';
 
-import 'app.dart';
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-void main() => runApp(const ShrineApp());
+  // TODO: Make a collection of cards (102)
+  // TODO: Add a variable for Category (104)
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Return an AsymmetricView (104)
+    // TODO: Pass Category variable to AsymmetricView (104)
+    return const Scaffold(
+      // TODO: Add app bar (102)
+      // TODO: Add a grid view (102)
+      body: Center(
+        child: Text('You did it!'),
+      ),
+      // TODO: Set resizeToAvoidBottomInset (101)
+    );
+  }
+}

37216e23c7ae573782e1cc1b567da2a8a5dd5b71
Yoonho Aaron Kim <yoonhok524@users.noreply.github.com>
2022-12-09 21:55:20 +0900

    6. Make a card collection

--- mdc_101/lib/home.dart
+++ mdc_101/lib/home.dart
@@ -13,11 +13,58 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shrine/model/product.dart';
+import 'package:shrine/model/products_repository.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
+    if (products.isEmpty) {
+      return [];
+    }
+
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+        locale: Localizations.localeOf(context).toString());
+    return products
+        .map((product) => Card(
+              child: Column(
+                children: [
+                  AspectRatio(
+                    aspectRatio: 18.0 / 11.0,
+                    child: Image.asset(
+                      product.assetName,
+                      fit: BoxFit.fitWidth,
+                      package: product.assetPackage,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          product.name,
+                          style: theme.textTheme.headline6,
+                          maxLines: 1,
+                        ),
+                        const SizedBox(height: 8.0),
+                        Text(
+                          formatter.format(product.price),
+                          style: theme.textTheme.subtitle2,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ))
+        .toList();
+  }
+
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
@@ -41,31 +88,7 @@ class HomePage extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
         // TODO: Build a grid of cards (102)
-        children: <Widget>[
-          Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                AspectRatio(
-                  aspectRatio: 18.0 / 11.0,
-                  child: Image.asset('assets/diamond.png'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Title'),
-                      const SizedBox(height: 8.0),
-                      Text('Secondary Text'),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
+        children: _buildGridCards(context),
       ),
       resizeToAvoidBottomInset: false,
       // TODO: Set resizeToAvoidBottomInset (101)
